3b1f1cda50f1f3ae1f8fe9aabe47601294bbd5c1
carlacentenor <carlacentenor@gmail.com>
2018-02-07 12:53:36 -0500

    input operativos

--- js/app.js
+++ js/app.js
@@ -1,58 +1,88 @@
-let btnSearch = $('#btn-search');
+let btnSearch = document.getElementById('btn-search');
+let btnRoute = document.getElementById('btn-route');
 
-btnSearch.on('click', searchPosition);
+let origin = document.getElementById('origin');
+let destiny = document.getElementById('destiny');
+let placeSearch, autocomplete;
+
+btnSearch.addEventListener('click', searchPosition);
+btnRoute.addEventListener('click', searchRoute);
 
 function initMap() {
-    let pos = {
-        lat: -12.020651498087096,
-        lng: -76.93456887128904
-    }
-    let map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 5,
-        center: pos
-    });
-    let marker = new google.maps.Marker({
-        position: pos,
-        map: map,
-        // animaciones en marcadores
-        // label : 'A',
-        title: "Estoy aqui",
-        animation: google.maps.Animation.DROP,
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  initAutocomplete();
+  
+  let pos = {
+    lat: -12.020651498087096,
+    lng: -76.93456887128904
+  }
+  let map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 5,
+    center: pos
+  });
+  let marker = new google.maps.Marker({
+    position: pos,
+    map: map,
+    // animaciones en marcadores
+    // label : 'A',
+    title: "Estoy aqui",
+    animation: google.maps.Animation.DROP,
 
-    });
-
-    let origin = $('#origin');
-    let destiny = $('#destiny');
-    new google.maps.places.Autocomplete(
-        (document.getElementById('btn-search')), {
-            types: ['geocode']
-        });
+  });
+  searchRoute(directionsService,directionsDisplay);
 }
+
+function initAutocomplete() {
+  
+  let autocompleteorigin = new google.maps.places.Autocomplete(origin);
+  let autocompleteDestiny = new google.maps.places.Autocomplete(destiny);
+
+
+}
+
+function searchRoute(directionsService, directionsDisplay){
+  directionsService.route({
+    origin: origin.value,
+    destination: destiny.value,
+    travelMode: 'DRIVING'
+  }, function(response, status) {
+    if (status === 'OK') {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Ingrese direcciones');
+    }
+  });
+}
+
+
+
+
 
 
 
 function searchPosition() {
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
-            let myPosition = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            }
-            let map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 18,
-                center: myPosition
-            });
-            let marker = new google.maps.Marker({
-                position: myPosition,
-                map: map,
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let myPosition = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      }
+      let map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 18,
+        center: myPosition
+      });
+      let marker = new google.maps.Marker({
+        position: myPosition,
+        map: map,
 
 
-            });
+      });
 
-        });
+    });
 
-    } else {
-        console.log('Su navegador no soporta Geolocalización')
-    }
+  } else {
+    console.log('Su navegador no soporta Geolocalización')
+  }
 }
